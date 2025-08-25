ad79232547230be972b800ed80ed53ee33f5a143
kimmh74 <zziipp.mw@gmail.com>
2025-08-25 15:49:37 +0900

    1
    1

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [vue()],
+    base: mode === 'production'  ?  '/vue-site/'  :  '/',        
+  }
 })
