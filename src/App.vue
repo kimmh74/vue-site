ffdeda78c092c235debd9c1313527d065bc3aebe
kimmh74 <zziipp.mw@gmail.com>
2025-08-26 16:06:24 +0900

    1
    1

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <script setup>
 
 function goTo1() {
-    window.location.href = "https://kimmh74.github.io/vue_day01_0822/";
+  window.location.href = "https://kimmh74.github.io/vue_day01_0822/";
 }
 function goTo2() {
-    window.location.href = "https://kimmh74.github.io/vue_day02_0825/";
+  window.location.href = "https://kimmh74.github.io/vue_day02_0825/";
+}
+function goTo3() {
+  window.location.href = "https://kimmh74.github.io/vue_day03_0826/";
 }
 
 </script>
 
 <template>
-    <div class="container">
-        <h1 class="note-title">나만의 멋진 Vue_STUDY 노트<span><p>st.kimmh</p></span></h1>
-        <div class="button-list">
-            <button @click="goTo1" class="note-button">첫번째 : day01_0822</button>
-              <button @click="goTo2" class="note-button">두번째 : day02_0825</button>
-            <button class="note-button">세번째 : 버튼</button>
-            <button class="note-button">네번째 : 버튼</button>
-        </div>
+  <div class="container">
+    <h1 class="note-title">나만의 멋진 Vue_STUDY 노트<span>
+        <p>st.kimmh</p>
+      </span></h1>
+    <div class="button-list">
+      <button @click="goTo1" class="note-button">첫번날 : day01_0822</button>
+      <button @click="goTo2" class="note-button">두번날 : day02_0825</button>
+      <button @click="goTo3" class="note-button">세째날 : day03_0825</button>      
     </div>
+  </div>
 
-    <!-- <button @click="goToNaver">네이버로 이동</button> -->
+  <!-- <button @click="goToNaver">네이버로 이동</button> -->
 
 </template>
 
@@ -33,12 +37,14 @@ function goTo2() {
   border-radius: 16px;
   box-shadow: 4px 6px 12px rgba(251, 176, 45, 0.4);
   font-family: 'Nanum Gothic', sans-serif;
-} 
+}
+
 /* 제목 스타일 */
 .note-title {
   font-size: 1.8em;
   font-weight: 700;
-  color: #f9a825; /* 황금빛 느낌 */
+  color: #f9a825;
+  /* 황금빛 느낌 */
   text-align: center;
   margin-bottom: 20px;
   padding: 10px 0;
@@ -46,14 +52,16 @@ function goTo2() {
   text-shadow: 1px 1px 2px rgba(246, 190, 0, 0.6);
   user-select: none;
 }
+
 /* 버튼목록 세로 정렬 + 간격 */
 .button-list {
   display: flex;
   flex-direction: column;
   gap: 12px;
 }
+
 .note-button:hover {
   background-color: #fff176;
-  box-shadow: 4px 8px 12px rgba(0,0,0,0.25);
+  box-shadow: 4px 8px 12px rgba(0, 0, 0, 0.25);
 }
 </style>
